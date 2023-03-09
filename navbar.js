a55e6c9f96cf8076433e4ad6dade4c32ae9fe7da
PaulDec <paul.decrosse@gmail.com>
2023-03-09 12:51:36 +0100

    update

--- navbar.js
+++ navbar.js
@@ -1,9 +1,15 @@
 function displayMenu() {
     let menu = document.getElementsByClassName("navbar-mobile")[0];
+    let open = document.getElementsByClassName("navbar-hamburger-open")[0];
+    let close = document.getElementsByClassName("navbar-hamburger-close")[0];
     style = window.getComputedStyle(menu);
     if (style.display == 'flex') {
         menu.style.display = 'none';
+        close.style.display = 'none';
+        open.style.display = 'flex'
     } else {
         menu.style.display = 'flex';
+        open.style.display = 'none'
+        close.style.display = 'flex';
     }
 }
